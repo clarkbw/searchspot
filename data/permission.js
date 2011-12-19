bbd6e84344ac19162b12436f0909488d63b53b67
Bryan Clark <clarkbw@gmail.com>
2011-12-19 14:31:47 -0800

    add an initial permission panel to ask users if it's ok that we share geolocation and send data usage back to mozilla

---
+++ data/permission.js
@@ -0,0 +1,22 @@
+//permission.js
+
+$(document).ready(function () {
+
+  $("#ok").click(function() {
+    self.port.emit("click", "ok" );
+    return false;
+  });
+
+  $("#not").click(function() {
+    self.port.emit("click", "not" );
+    return false;
+  });
+
+  $("#not").mouseover(function() {
+    $("#otherwise").addClass("scary");
+  }).mouseout(function() {
+    $("#otherwise").removeClass("scary");
+  });
+  self.port.emit("resize", { "width" : $("body").width(), "height" : $("body").height() });
+
+});
