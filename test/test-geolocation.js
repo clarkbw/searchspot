5fce588b0354cd005a6448946bba9564266645d8
Bryan Clark <clarkbw@gmail.com>
2011-12-20 11:48:07 -0800

    everything isn't working 100% but this is a good time to checkpoint the new geocode module

--- test/test-geolocation.js
+++ test/test-geolocation.js
@@ -8,13 +8,24 @@ exports.test_allowed = function(test) {
   test.assert(!Geolocation.allowed, "Geolocation shouldn't be allowed by default");
 };
 
-exports.test_monitor = function(test) {
+exports.test_monitor_coordinates = function(test) {
   Geolocation.timeout = 5 * 1000;
-  Geolocation.once("position", function(position) {
-    test.pass("Found position");
+  Geolocation.allowed = false;
+  Geolocation.once("coords", function(position) {
+    test.pass("Found coordinates");
     test.done();
   });
   Geolocation.allowed = true;
-  test.waitUntilDone(Geolocation.timeout);
+  test.waitUntilDone(Geolocation.timeout * 2);
 };
 
+exports.test_monitor_address = function(test) {
+  Geolocation.timeout = 5 * 1000;
+  Geolocation.allowed = false;
+  Geolocation.once("address", function(position) {
+    test.pass("Found address");
+    test.done();
+  });
+  Geolocation.allowed = true;
+  test.waitUntilDone(Geolocation.timeout * 2);
+};
