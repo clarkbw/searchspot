499fa98e64c40a757af7c1c7e09facb96e336a7f
Bryan Clark <clarkbw@gmail.com>
2011-12-27 12:38:09 -0800

    improved the geolocation testing and option setting

--- test/test-geolocation.js
+++ test/test-geolocation.js
@@ -8,21 +8,13 @@ exports.test_allowed = function(test) {
   test.assert(!Geolocation.allowed, "Geolocation shouldn't be allowed by default");
 };
 
-exports.test_monitor_coordinates = function(test) {
+exports.test_monitor_coordinates_and_address = function(test) {
   Geolocation.timeout = 5 * 1000;
   Geolocation.allowed = false;
   Geolocation.once("coords", function(position) {
     test.pass("Found coordinates");
-    test.done();
   });
-  Geolocation.allowed = true;
-  test.waitUntilDone(Geolocation.timeout * 2);
-};
-
-exports.test_monitor_address = function(test) {
-  Geolocation.timeout = 5 * 1000;
-  Geolocation.allowed = false;
-  Geolocation.once("address", function(position) {
+  Geolocation.once("address", function() {
     test.pass("Found address");
     test.done();
   });
