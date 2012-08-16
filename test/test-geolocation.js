d6044429cf1adb596d1b8963ad6659be18840ed7
Bryan Clark <clarkbw@gmail.com>
2012-08-16 16:18:16 -0700

    remove useless unit test and add the license header

--- test/test-geolocation.js
+++ test/test-geolocation.js
@@ -1,8 +1,9 @@
-const { Geolocation } = require("geolocation");
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
 
-exports.test_test_run = function(test) {
-  test.pass("Unit test running!");
-};
+const { Geolocation } = require("geolocation");
 
 exports.test_allowed = function(test) {
   test.assert(!Geolocation.allowed, "Geolocation shouldn't be allowed by default");
