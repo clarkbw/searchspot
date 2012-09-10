1b9116796d520766eb262cac56a446af72594658
Bryan Clark <clarkbw@gmail.com>
2012-09-10 15:20:08 -0700

    code fix for pagemod

--- data/search-engines-collector-pagemod.js
+++ data/search-engines-collector-pagemod.js
@@ -1,10 +1,12 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
 * License, v. 2.0. If a copy of the MPL was not distributed with this
 * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 /*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true,
   strict:true, undef:true, curly:true, browser:true, es5:true,
   indent:2, maxerr:50, devel:true, node:true, boss:true, white:true,
   globalstrict:true, nomen:true, newcap:true*/
+
 /*global self:false */
 
 //"use strict";
@@ -20,8 +22,8 @@ var rel = "search",
 
 for (var i = 0, link; link = links[i]; i += 1) {
   if (link.nodeName === nodeName && link.rel === rel && link.type === type) {
-    title = links[i].getAttribute("title");
-    href = links[i].getAttribute("href");
+    title = link.getAttribute("title");
+    href = link.getAttribute("href");
     if (title !== null && href !== null) {
       results.push({ site : document.URL, name : title, opensearch : href });
     }
