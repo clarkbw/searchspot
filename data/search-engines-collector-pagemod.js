83288f922f0bda202f39e0fed4e98b637681866f
Bryan Clark <clarkbw@gmail.com>
2012-09-10 10:50:28 -0700

    using jshint for the page-mod and better value checking to ensure we don't hit errors.  wrote up some docs too.  added charset to the fixtures page.

--- data/search-engines-collector-pagemod.js
+++ data/search-engines-collector-pagemod.js
@@ -1,29 +1,33 @@
-// search-engine-pagemod.js
-//
-// This file scans pages looking for opensearch description urls
-// returns an object that can be passed to the system search engine service for
-// parsing.
-//
-// example { site : "http://google.com/path/to/random/search",
-// title : "Google Search",
-// engine : "/google.xml" }
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true,
+  strict:true, undef:true, curly:true, browser:true, es5:true,
+  indent:2, maxerr:50, devel:true, node:true, boss:true, white:true,
+  globalstrict:true, nomen:true, newcap:true*/
+/*global self:false */
 
-var links = null, i = null, results = [];
+//"use strict";
 
-links = document.querySelectorAll("link[rel=search][type='application/opensearchdescription+xml']");
-//console.log(links);
+var rel = "search",
+    type = "application/opensearchdescription+xml",
+    nodeName = "LINK",
+    selector = nodeName + "[rel='" + rel + "'][type='" + type + "']",
+    links = document.querySelectorAll(selector),
+    title = null,
+    href = null,
+    results = [];
 
-//console.log("site", document.URL);
-for (i in links) {
-  if (links[i] && typeof links[i].getAttribute !== "undefined") {
-    var title = links[i].getAttribute("title");
-    var href = links[i].getAttribute("href");
-    if (title && href) {
+for (var i = 0, link; link = links[i]; i += 1) {
+  if (link.nodeName === nodeName && link.rel === rel && link.type === type) {
+    title = links[i].getAttribute("title");
+    href = links[i].getAttribute("href");
+    if (title !== null && href !== null) {
       results.push({ site : document.URL, name : title, opensearch : href });
     }
   }
-
 }
+
 if (results.length > 0) {
   self.postMessage(results);
 }
---
+++ docs/search-engines-collector-pagemod.md
@@ -0,0 +1,25 @@
+<!-- This Source Code Form is subject to the terms of the Mozilla Public
+   - License, v. 2.0. If a copy of the MPL was not distributed with this
+   - file, You can obtain one at http://mozilla.org/MPL/2.0/. -->
+
+The `search-engines-collector-pagemod` module is a simple 
+[page-mod](packages/addon-kit/page-mod.html) for finding Open Search link
+references within a page.
+
+The module looks at all pages for `link` elements with the attributes
+`rel='search'` and `type='application/opensearchdescription+xml'`.
+
+For every site with Open Search link references this module will send back an
+array of relevant link element data.
+
+An example of the data sent through postMessage looks like this:
+
+`[ { 'site': document.URL, 'name' : title, 'opensearch' : href } ]`
+
+Where 
+  `site` = "http://google.com/path/to/random/search"
+  `title` = "Google Search"
+  `opensearch` = "/google.xml"
+
+Note that the `opensearch` entry can be relative which is why you need the
+`site` entry to create a proper URL.
