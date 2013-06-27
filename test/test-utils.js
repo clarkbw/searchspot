f0076dcc703129b29113acf27db55c7d093d718b
Bryan Clark <clarkbw@gmail.com>
2013-06-27 12:33:53 -0700

    test utils addXULStylesheet improvements
    move to just generically test the addXULStylesheet function from the
    utils module and test that it goes away on unload.

--- test/test-utils.js
+++ test/test-utils.js
@@ -2,26 +2,30 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true,
-  strict:true, undef:true, curly:true, browser:false, es5:true,
-  indent:2, maxerr:50, devel:true, node:true, boss:true, white:true,
-  globalstrict:true, nomen:false, newcap:true, esnext: true */
+ /*jshint esnext:true, node:true, newcap:false */
 
-'use strict';
+"use strict";
 
-var utils = require('utils');
+var { Loader } = require('sdk/test/loader');
+
 var chrome = require("chrome");
 var data = require('sdk/self').data;
 
-exports['test addXULStylesheet'] = function (assert, done) {
-  var main = require('main');
-  main.main();
-  var ssuri = utils.newURI(data.url("searchspot-style.css"));
+exports['test addXULStylesheet'] = function (assert) {
+  var loader = new Loader(module),
+      utils = loader.require("./utils");
+
+  var ssurl = data.url("searchspot-style.css");
+  utils.addXULStylesheet(ssurl);
+
+  var ssuri = utils.newURI(ssurl);
   var sss = chrome.Cc["@mozilla.org/content/style-sheet-service;1"]
                   .getService(chrome.Ci.nsIStyleSheetService);
 
-  assert.equal(sss.sheetRegistered(ssuri, sss.AGENT_SHEET), true, "style sheet is registered");
-  done();
+  assert.ok(sss.sheetRegistered(ssuri, sss.AGENT_SHEET), "style sheet is registered");
+
+  loader.unload();
+  assert.ok(!sss.sheetRegistered(ssuri, sss.AGENT_SHEET), "style sheet is no longer registered");
 };
 
 require('test').run(exports);
